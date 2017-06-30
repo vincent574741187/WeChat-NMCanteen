392ad7cf9d87df40bb1616b909e599c5e669aaf2
Vincent Wen <vincent574741187@gmail.com>
2017-06-30 17:19:39 +0800

    完善提交订餐功能

--- pages/order/confirm/confirm.js
+++ pages/order/confirm/confirm.js
@@ -3,32 +3,42 @@ Page({
     data: {
         showTopTips: false,
         date: util.formatDate(new Date),
-        countries: ["早餐", "午餐", "晚餐"],
-        countryIndex: 0,
-
+        mealTypes: ["早餐", "午餐", "晚餐"],
+        mealTypesIndex: 0
     },
-    showTopTips: function(){
-        var that = this;
-        this.setData({
-            showTopTips: true
-        });
-        setTimeout(function(){
-            that.setData({
-                showTopTips: false
-            });
-        }, 3000);
-    },
-    bindDateChange: function (e) {
-      this.setData({
-        date: e.detail.value
-      })
-    },
-    bindCountryChange: function(e) {
-        console.log('picker country 发生选择改变，携带值为', e.detail.value);
-
-        this.setData({
-            countryIndex: e.detail.value
-        })
-    }
+  formSubmit: function (e) {
+    console.log('form发生了submit事件，携带数据为：', e.detail.value)
+    wx.request({
+      url: 'https://51399814.qcloud.la/simpleServer/MyHttpServlet',
+      data: {
+        serviceName:"orderService",
+        mealType: e.detail.value.mealType,
+        mealDate: e.detail.value.mealDate,
+        mealNums: e.detail.value.mealNums
+      },
+      header: {
+        'content-type': 'application/json'
+      },
+      complete: function (res) {
+        if (res.data == "success"){
+          wx.showToast({
+            title: '订餐成功',
+            icon: 'success',
+            duration: 3000
+          });
+        }else{
+          wx.showModal({
+            content: res.data,
+            showCancel: false,
+            success: function (res) {
+              if (res.confirm) {
+                console.log('用户点击确定')
+              }
+            }
+          });
+        }
+      }
+    })
+  }
 
 });
